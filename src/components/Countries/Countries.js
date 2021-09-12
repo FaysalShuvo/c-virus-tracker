76f3dfaf79d5b6eee3e96889ec6e5290a39f060e
faysalshuvo <faysalshuvo.dev@gmail.com>
2021-09-12 18:53:19 +0600

    initial country setup

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import styles from "./Country.module.css";
 
+import { fetchCountries } from "../../api";
 const Countries = () => {
+  const [fetchCountries, setFetchedCountries] = useState([]);
+  useEffect(() => {
+    const fetchCountries = async () => {
+      setFetchedCountries(await fetchCountries);
+    };
+    fetchCountries();
+  }, [setFetchedCountries]);
+
   return (
-    <div>
-      <h1>Countries</h1>
-    </div>
+    <FormControl className={styles.formControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+      </NativeSelect>
+    </FormControl>
   );
 };
 
